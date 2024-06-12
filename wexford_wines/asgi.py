cea236615526826909865b82b50b276d297724d0
Nicola Wallace <nfewallace@gmail.com>
2024-06-12 21:26:21 +0000

    Start django project and app, setup heroku and postgres

---
+++ wexford_wines/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for wexford_wines project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wexford_wines.settings')
+
+application = get_asgi_application()
