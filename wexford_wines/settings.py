3ec043b14bcf689ce73ca3294157f0f29afa0d0d
Nic-Wallace <nfewallace@gmail.com>
2025-02-24 20:51:14 +0000

    Remove allauth socials from templates

--- wexford_wines/settings.py
+++ wexford_wines/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
