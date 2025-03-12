c7485231f29ae03bae2729fb6d2d634034c5e2cd
Nic-Wallace <nfewallace@gmail.com>
2025-03-12 18:14:35 +0000

    Initial indiv. wine listing views and templates

--- wine/views.py
+++ wine/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 
 # Create your views here.
@@ -10,7 +10,19 @@ def all_wines(request):
     wine = Listing.objects.all()
 
     context = {
-        'wine': wine,
+        'wines': wines,
     }
 
     return render(request, 'wine/wine.html', context)
+
+
+def wine_listing(request, product_id):
+    """ View to return a singular wine listing """
+
+    wine = get_object_or_404(Wine, pk=product_id)
+
+    context = {
+        'wine': wine,
+    }
+
+    return render(request, 'wine/wine_listing.html', context)
