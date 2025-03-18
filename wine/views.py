f87ec1b73801c94b66724dcfe708a78eaa78e812
Nic-Wallace <nfewallace@gmail.com>
2025-03-18 17:46:30 +0000

    Add site search functionality

--- wine/views.py
+++ wine/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Listing
 
 # Create your views here.
@@ -8,9 +10,21 @@ def all_wines(request):
     """ View to return the wine listings """
 
     wines = Listing.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter search term")
+                return redirect(reverse('wines'))
+            
+            queries = Q(name__icontains=query)
+            wines = wines.filter(queries)
 
     context = {
         'wines': wines,
+        'search_term': query,
     }
 
     return render(request, 'wine/wine.html', context)
