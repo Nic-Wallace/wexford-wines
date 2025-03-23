661530836e8a3f1a67ad0079f16fa7670b91f3f7
Nic-Wallace <nfewallace@gmail.com>
2025-03-23 19:35:31 +0000

    Add ability to put items in cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cart_view, name='cart_view')
+    path('', views.cart_view, name='cart_view'),
+    path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
 ]
