a3700fa0d0a163fd8659a8d0f72203b5d56a127d
Nic-Wallace <nfewallace@gmail.com>
2025-03-23 23:38:28 +0000

    Build on shopping cart functionality

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,23 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from wine.models import Listing
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     wine_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        wine = get_object_or_404(Listing, pk=item_id)
+        total += quantity * wine.price
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'wine': wine,
+        })
 
     context = {
         'cart_items': cart_items,
