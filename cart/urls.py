484b9a6a915395812d128d6b3b6ef2d9443ca7dd
Nic-Wallace <nfewallace@gmail.com>
2025-03-28 19:14:53 +0000

    Attempt fix of cart item controls

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.cart_view, name='cart_view'),
     path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
+    path('adjust/<item_id>', views.adjust_cart, name='adjust_cart'),
+    path("remove/<item_id>/", views.remove_from_cart, name="remove_from_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from wine.models import Listing
 
-# Create your views here.
 
 def cart_view(request):
     """ View to return the shopping cart """
     return render(request, 'cart/cart.html')
 
+
 def add_to_cart(request, item_id):
-    """ Add a quantity of the specified product to the cart """
+    """ Add a quantity of the specified wine to the cart """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -20,3 +21,58 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """ Adjust the quantity of a wine """
+
+    quantity = int(request.POST.get("quantity"))
+    cart = request.session.get("cart", {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session["cart"] = cart
+    return redirect(reverse("cart_view"))
+
+
+# def remove_from_cart(request, item_id):
+#     """ Remove the item from the shopping cart """
+
+#     cart = request.session.get("cart", {})
+#     cart.pop(item_id)
+
+#     request.session["cart"] = cart
+#     return HttpResponse(status=200)
+
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+
+    wine = get_object_or_404(Listing, pk=item_id)
+    try:
+        size = None
+        if 'wine_size' in request.POST:
+            size = request.POST['wine_size']
+        cart = request.session.get('cart', {})
+
+        if size:
+            del cart[item_id]["items_by_size"][size]
+            if not cart[item_id]["items_by_size"]:
+                cart.pop(item_id)
+        else:
+            cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(f"Error removing item: {e}")
+        return HttpResponse(status=500)
