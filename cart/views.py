ae3b79932f9818ab5c9bc03d56901bf86c1ce50c
Nic-Wallace <nfewallace@gmail.com>
2025-04-14 18:43:34 +0100

    Add toast functionality to wine actions

--- cart/views.py
+++ cart/views.py
@@ -18,6 +18,7 @@ def add_to_cart(request, item_id):
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated quantity of {wine.name} in cart')
     else:
         cart[item_id] = quantity
         messages.success(request, f'Added {wine.name} to cart')
@@ -39,8 +40,10 @@ def adjust_cart(request, item_id):
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated quantity of {wine.name} in cart')
     else:
         cart.pop(item_id)
+        messages.success(request, f'Removed {wine.name} from cart')
 
     request.session["cart"] = cart
     return redirect(reverse("cart_view"))
@@ -57,7 +60,7 @@ def adjust_cart(request, item_id):
 
 
 def remove_from_cart(request, item_id):
-    """Remove the item from the shopping cart"""
+    """Remove a wine from the shopping cart"""
 
     wine = get_object_or_404(Listing, pk=item_id)
     try:
@@ -72,9 +75,11 @@ def remove_from_cart(request, item_id):
                 cart.pop(item_id)
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed {wine.name} from cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
+
     except Exception as e:
-        messages.error(f"Error removing item: {e}")
+        messages.error(f"Error removing wine: {e}")
         return HttpResponse(status=500)
