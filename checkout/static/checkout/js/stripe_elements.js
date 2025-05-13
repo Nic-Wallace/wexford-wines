ff608336e9346bbfaa3fcf65f79ce57276d8ab38
Nic-Wallace <nfewallace@gmail.com>
2025-05-13 18:13:42 +0100

    Add details to stripe payment form

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -48,8 +48,30 @@ form.addEventListener('submit', function(ev) {
     stripe.confirmCardPayment(client_secret, {
         payment_method: {
             card: card,
+            billing_details: {
+                name: $.trim(form.full_name.value),
+                email: $.trim(form.email.value),
+                phone: $.trim(form.phone_number.value),
+                address:{
+                    country: $.trim(form.country.value),
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    county: $.trim(form.county.value),
+                }
         }
-    }).then(function(result) {
+    },
+    shipping_details: {
+        name: $.trim(form.full_name.value),
+        phone: $.trim(form.phone_number.value),
+        address:{
+            country: $.trim(form.country.value),
+            postcode: $.trim(form.postcode.value),
+            line1: $.trim(form.street_address1.value),
+            line2: $.trim(form.street_address2.value),
+            county: $.trim(form.county.value),
+        }
+    },
+}).then(function(result) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
             var html = `
