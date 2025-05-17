ba4ec25252696b827bfee8329bd99fe0f8f291fc
Nic-Wallace <nfewallace@gmail.com>
2025-05-17 17:56:16 +0100

    Add country dropdown to checkout form

--- checkout/models.py
+++ checkout/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from wine.models import Listing
+from django_countries.fields import CountryField
 
 
 class Order(models.Model):
@@ -9,11 +10,11 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country*', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=80, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
