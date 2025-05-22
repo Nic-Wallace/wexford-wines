402de470b7b463ee6a9a918753292a972b0d65d6
Nic-Wallace <nfewallace@gmail.com>
2025-05-22 15:35:13 +0100

    Add contact app content

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact(request):
+    """
+    contact view
+    """
+    form = ContactForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Form submitted!')
+            return redirect('homepage')
+        else:
+            messages.error(request, 'ERROR, please try again.')
+    template = 'contact/contact.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
