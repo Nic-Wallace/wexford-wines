b30d9b106084a208f6c7d194c4adbcf791499efc
Nic-Wallace <nfewallace@gmail.com>
2025-05-22 15:36:06 +0100

    Add FAQ app content

---
+++ faqs/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import FAQ
+
+
+class FAQForm(forms.ModelForm):
+
+    class Meta:
+        model = FAQ
+        fields = '__all__'
--- faqs/views.py
+++ faqs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import FAQ
 
-# Create your views here.
+
+def faqs(request):
+    """
+    FAQs view
+    """
+    faqs = FAQ.objects.all()
+    template = 'faqs/faqs.html'
+    context = {
+        'faqs': faqs,
+    }
+    return render(request, template, context)
