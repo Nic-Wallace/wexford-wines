66034aa30c0d863372c16c779aa57c993297f607
Nic-Wallace <nfewallace@gmail.com>
2025-05-22 15:35:41 +0100

    Add newsletter app content

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import NewsletterForm
 
-# Create your views here.
+
+def newsletter(request):
+    """
+    newsletter view
+    """
+    form = NewsletterForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Subscribed to newsletter!')
+            return redirect('homepage')
+        else:
+            messages.error(request, 'ERROR, please try again.')
+    template = 'newsletter/newsletter.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
