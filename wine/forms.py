8437f80dc769f5b83ba9c552a763e0481613f37d
Nic-Wallace <nfewallace@gmail.com>
2025-05-22 14:23:20 +0100

    Refactor wine listing management

--- wine/forms.py
+++ wine/forms.py
@@ -16,20 +16,15 @@ class WineForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         placeholders = {
             'name': 'Name of wine',
-            'wine_colour': 'Wine colour',
-            'country': 'Country of origin',
-            'taste_profile': 'Taste profile',
-            'Price': '€',
         }
 
         for field in self.fields:
-            if field != 'default_country':
+            if field in placeholders:
                 if self.fields[field].required:
                     placeholder = f'{placeholders[field]} *'
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False
             for field_name, field in self.fields.items():
                 field.widget.attrs['class'] = 'border-black listing-form-input'
