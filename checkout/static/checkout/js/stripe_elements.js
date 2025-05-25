5a1103897d2822823c52a868cdcb721ef344be8d
Nic-Wallace <nfewallace@gmail.com>
2025-05-25 23:54:13 +0100

    Amend js files for jshint testing

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 11, jquery: true */
+
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
@@ -103,6 +105,6 @@ form.addEventListener('submit', function(ev) {
         });
     }).fail(function () {
         location.reload();
-    })
+    });
 
 });
